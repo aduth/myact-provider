58516c632565c36d4981e608f9d54b576dfad565
Andrew Duthie <aduthie7@gmail.com>
2014-11-03 21:56:02 -0500

    Rename Provider to MyactProvider

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var EventEmitter = require( 'events' ).EventEmitter;
 
-var Provider = module.exports = function( options ) {
+var MyactProvider = module.exports = function( options ) {
     if ( 'object' !== typeof options ) options = {};
     this.options = options;
 };
 
-Provider.prototype = Object.create( EventEmitter.prototype );
+MyactProvider.prototype = Object.create( EventEmitter.prototype );
 
-Provider.prototype.interval = true;
+MyactProvider.prototype.interval = true;
 
-Provider.prototype.invoke = function() {};
+MyactProvider.prototype.invoke = function() {};
